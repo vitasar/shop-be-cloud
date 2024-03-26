2d7335d2240ccfe81010f7e1f1d113d5ddd21a78
Vitaly Bubyakin <vitaly_bubyakin@epam.com>
2024-03-26 23:58:00 +0300

    initial setup: products lambda in two base serverless configurations

---
+++ product-service/serverless.ts
@@ -0,0 +1,40 @@
+import type { AWS } from '@serverless/typescript';
+
+import getProductsList from '@functions/getProductsList';
+
+const serverlessConfiguration: AWS = {
+  service: 'product-service',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs20.x',
+    stage: 'dev',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: { getProductsList },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node20',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
